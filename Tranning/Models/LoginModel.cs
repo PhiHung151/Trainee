4aea9001c1b24d5a9c9ef2962f8647138e82bd71
Nomm <94917416+PhiHung151@users.noreply.github.com>
2023-12-13 13:25:58 +0700

    first commit

---
+++ Tranning/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Tranning.Models
+{
+    public class LoginModel
+    {
+        // day la noi dinh nghia cac truong du lieu can thao tac trong bang users (vi phan login lien quan den bang users)
+        public string UserID { get; set; }
+        public int RoleID { get; set; }
+        public string Username { get; set; }
+        public string EmailUser { get; set; }
+        public string PhoneUser { get; set; }
+        
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public string Status { get; set; }
+    }
+}
